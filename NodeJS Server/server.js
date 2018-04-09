a36594d9dee3c42b452cca6d4cbfcf888b06a2e3
Lasha Zakariashvili <Lzak95@ymail.com>
2018-04-09 09:34:08 -0400

    Added NodeJS backend and updated README.

---
+++ NodeJS Server/server.js
@@ -0,0 +1,104 @@
+//BitCamp 2018
+
+
+var app = require("express")();
+var server = require('http').createServer(app);
+var debug = require('debug')('debug');
+var WebSocket = require('ws');
+var requests = require("request");
+
+app.use(require('express').static('public'));
+const wss = new WebSocket.Server({server});
+
+//Public Data (bite me)
+client_coords = [];
+
+const getRandom = (min, max) => Math.random() * (max - min) + min;
+function getPoint() {
+  const x = getRandom(-73.975, -73.990);
+  const y = getRandom(40.718, 40.732);
+  const date = Date.now();
+  const point = {
+	tag: "add_point",
+    geometry: {
+      "x": x,
+      "y": y
+    },
+    attributes: {
+      "timeStamp": date
+    }
+  };
+  return JSON.stringify(point);
+}
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => {
+    if(client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+setInterval(()=> {
+	wss.broadcast(getPoint());
+	debug("Updated Point");
+}, 1000);
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+	  //ws.send(message)
+	  message = JSON.parse(message);
+	  console.log(typeof(message));
+	  if (message.tag == "assign_point") {
+			console.log('received: %s', message.tag);
+		  	console.log(message.coords);
+			console.log(message.coords.x);
+			client_coords.push(message.coords)
+
+			requests({
+				url:"https://maps.googleapis.com/maps/api/geocode/json?latlng=" + message.coords.y + "," + message.coords.x + "&key=AIzaSyDeBMwodkWHr82lBFMjIJEBFrnZnXTLSg8",
+				json: true
+			}, function (error, res, body) {
+				//console.log(JSON.parse(body).results[0].formatted_address);
+				console.log(body);
+			});
+
+			var stuff = {
+				tag: message.tag,
+				address: body.results[0].formatted_address,
+				coords: {
+					x: message.coords.x,
+					y: message.coords.y
+				}
+			}
+		  wss.broadcast(JSON.stringify(stuff));
+	  }
+
+  });
+});
+/*
+wss.on('message', function incoming(data) {
+	debug("GOT DATA!");
+	debug(data);
+});
+*/
+/*
+ws.on('client get coords', function incoming() {
+  console.log(data);
+});
+*/
+
+app.get('/city', (req,res) => {
+	res.sendfile("index.html");	
+});
+
+app.get('/client', (req,res) => {
+	res.sendfile("client.html");	
+});
+
+
+server.listen(1337, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("Server running on %s:%s", host, port);
+});
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Pothole
+# Holey Moley
+
+##Inspiration
+We really wanted to put our skills to the limit this hackathon. We wanted to see if we could use image classification to identify potholes, send that information to the city, and assign that work onto a worker's phone.
+
+##What it does
+It sends video feed from the front of the car to IBM Watson, then based on whether it's a pot hole or not, we move on and send the information to Esri. Then a use would be able to view all potholes and assign a task to their constituents.
+
+##How we built it
+Using python, we take instant video, turn them into image frame that are sent into an image classification model. Then depending on degree of certainty, we send that data to Argcis, which allows all of that data to be displayed and manipulated. Then we created a client app which facilitates a service that allows them to "complete the job".
+
+##Built With
+Esri, IBM Watson, Node.JS, Python
