4fe4b608b6bd72d248a1b69dd9ce47b2a9f5b995
Alexa Rockwell <arkwl@Alexas-MacBook-Pro.local>
2018-04-11 16:35:42 -0400

    updated python scripts

--- bluemix_esri_posts.py
+++ bluemix_esri_posts.py
@@ -2,48 +2,47 @@ import os
 import json
 from watson_developer_cloud import VisualRecognitionV3
 
+from arcgis.gis import GIS
+from arcgis.features import Feature, FeatureSet, FeatureLayer
+from arcgis.geometry import Point
+
 #for every file pair in directory
-directory = os.fsencode('/Users/arkwl/Desktop/Workstation/Pothole/images/')
+directory = os.fsencode('./images/')
 
-#while True:
-if True:
-    #for file in os.listdir(directory):
-    for i in range(1):
-        '''
-        filename = os.fsdecode(file)
-        if filename.endswith(".jpg"):
-            #send photo to ibm bluemix
-            visual_recognition = VisualRecognitionV3(
-        	   '2016-05-20',
-        	   api_key='91bef4426dc30ea321530c574bb1c1e5af087243'
-            )
+for file in os.listdir(directory):
+    #for i in range(1):
+    filename = os.fsdecode(file)
+    if filename.endswith(".jpg"):
+        #send photo to ibm bluemix
+        print(filename)
+        visual_recognition = VisualRecognitionV3(
+            '2016-05-20',
+        	 api_key='##########################'
+             )
 
-            with open("/Users/arkwl/Desktop/Workstation/Pothole/images/"+filename, 'rb') as images_file:
-                classes = visual_recognition.classify(
-                    images_file,
-                    parameters=json.dumps({
-                        'classifier_ids': ['Potholes_971110931'],
-                        'threshold': 0.65
-                    }))
-            '''
+        with open("./images/"+filename, 'rb') as images_file:
+            classes = visual_recognition.classify(
+                images_file,
+                parameters=json.dumps({
+                    'classifier_ids': ['##########'],
+                    'threshold': 0.876
+                }))
 
-        from arcgis.gis import GIS
-        from arcgis.features import Feature, FeatureSet, FeatureLayer
-        from arcgis.geometry import Point
-        gis = GIS(username="lzak95", password="bitcamp2018")
-        p = Point({"x": "-73.3432", "y":  "30.6782"})
-        a = {"pothole_layer": "pothole"}
-        f = Feature(p, a)
-        fs = FeatureSet([f])
-        lyr = FeatureLayer("https://services8.arcgis.com/x660UqfqVJlbWB0Y/arcgis/rest/services/pothole_layers/FeatureServer/0", gis=gis)
-        lyr.edit_features(adds=fs)
+        #print(json.dumps(classes, indent=2))
+        data = json.loads(json.dumps(classes, indent=2))
+        if len(data['images'][0]['classifiers'][0]['classes']) != 0:
 
-            #get results
-            #data = json.dumps(classes, indent=2)
-            #print(data)
+            print(json.dumps(classes, indent=2))
+
+            f = open("./images/"+filename.replace("photo", "coor").replace("jpg", "txt"), "r")
+            d = float(f.read()) * 0.000000301
+
+            gis = GIS(username="lzak95", password="bitcamp2018")
+            p = Point({"x": -73.471977 + d, "y": 40.703342})
+            a = {"pothole_layer": "pothole"}
+            f = Feature(p, a)
+            fs = FeatureSet([f])
+            lyr = FeatureLayer("https://services8.arcgis.com/x660UqfqVJlbWB0Y/arcgis/rest/services/pothole_layers/FeatureServer/0", gis=gis)
+            lyr.edit_features(adds=fs)
 
         #delete photo and txt
-        #os.remove("/Users/arkwl/Desktop/Workstation/Pothole/images/"+filename)
-
-    #else:
-        #continue
